fcd6b0c084f9c2283fee09d56be6ca23de1f6708
David A Roberts <d@vidr.cc>
2015-09-21 20:33:38 +1000

    first commit

--- scraper.py
+++ scraper.py
@@ -1,21 +1,32 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
+
+for url in open('urls.txt','r'):
+	try:
+		# Read in a page
+		url = url.strip()
+		html = scraperwiki.scrape(url)
+
+		# Find something on the page using css selectors
+		root = lxml.html.fromstring(html)
+		pdfs = set()
+		for a in root.cssselect('a'):
+			href = a.get('href')
+			if href and 'pdf' in href: pdfs.add(href)
+		print url, pdfs
+
+		# Write out to the sqlite database using scraperwiki library
+		for pdf in pdfs:
+			scraperwiki.sqlite.save(unique_keys=['pdf'], data={"pdf": pdf, "url": url})
+	except Exception as e:
+		print e
+		#break
+
+# An arbitrary query against the database
+#print scraperwiki.sql.select("* from data")
 
 # You don't have to do things with the ScraperWiki and lxml libraries.
 # You can use whatever libraries you want: https://morph.io/documentation/python
